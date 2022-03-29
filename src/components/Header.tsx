da2a04f01a8405488827c57cd9136ccc052168e6
ToshaBY <anton.rybakou@gmail.com>
2022-03-30 01:05:19 +0300

    feat: 13 video (css modules)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import s from './Header.module.css';
 
 export const Header: React.FC<any> = (props) => {
-  return (
-      <div className="header">
-          <img src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png" alt={"Logo image"} />
-      </div>
-  )
+    return (
+        <div className={s.header}>
+            <img src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png"
+                 alt={"Logo image"}/>
+        </div>
+    )
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 export const Profile: React.FC<any> = (props) => {
-  return (
-      <div className={"content"}>
-          <img src="https://www.teahub.io/photos/full/225-2258573_outer-space-minimalist-wallpaper-hd.jpg" alt={"Some image"}/>
-          <div>ava + description</div>
-          <div>My posts</div>
-          <div>New post</div>
-          <div>Post1</div>
-          <div>Post2</div>
-      </div>
-  )
+    return (
+        <div className={s.content}>
+            <img src="https://www.teahub.io/photos/full/225-2258573_outer-space-minimalist-wallpaper-hd.jpg"
+                 alt={"Some image"}/>
+            <div>ava + description</div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div className={s.posts}>
+                    <div className={s.item}>Post1</div>
+                    <div className={s.item}>Post2</div>
+                </div>
+            </div>
+
+        </div>
+    )
 }
