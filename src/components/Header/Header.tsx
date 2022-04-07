44a50da4fec509d9a9449d279d3fba31a4bd7a73
ToshaBY <anton.rybakou@gmail.com>
2022-04-07 20:21:39 +0300

    refactor: remove loader

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,16 +4,10 @@ import s from './Header.module.css';
 export const Header: React.FC<any> = (props) => {
     return (
         <>
-        <div className={s.header}>
-            <img src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png"
-                 alt={"Logo image"}/>
-        </div>
-        <div className={s.loader_wrapper}>
-            <div className={s.loader}>
-                <div className={s.loader_inner}/>
+            <div className={s.header}>
+                <img src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png"
+                     alt={"Logo image"}/>
             </div>
-        </div>
-
         </>
     )
 }
