a32b2960549d0aa474e5ac806ccb2de5b9576a45
ToshaBY <anton.rybakou@gmail.com>
2022-04-08 13:26:52 +0300

    feat: video 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,38 @@ import s from './Dialogs.module.css';
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Name1
+                </div>
+                <div className={s.dialog}>
+                    Name2
+                </div>
+                <div className={s.dialog}>
+                    Name3
+                </div>
+                <div className={s.dialog}>
+                    Name4
+                </div>
+                <div className={s.dialog}>
+                    Name5
+                </div>
+                <div className={s.dialog}>
+                    Name6
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    message1
+                </div>
+                <div className={s.message}>
+                    message2
+                </div>
+                <div className={s.message}>
+                    message3
+                </div>
+            </div>
         </div>
     );
 };
