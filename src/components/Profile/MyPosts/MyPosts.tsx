1decc4876e1e19c85971462f1793fa408cd6e404
ToshaBY <anton.rybakou@gmail.com>
2022-05-05 22:50:20 +0300

    feat: video 31

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {RefObject} from 'react';
 import s from './MyPosts.module.css'
 import {Post} from "./Post/Post";
 import {PostsDataType} from "../../../redux/state";
@@ -10,16 +10,28 @@ type MyPostsType = {
 export const MyPosts: React.FC<MyPostsType> = (props) => {
     // Function to render all posts from data (map)
     const postsList = props.postsData.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
+    // Create a reference to text in the text area
+    const newPostElement = React.createRef<HTMLTextAreaElement>()
+    // Callback function for button to add post
+    const addPost = () => {
+        if (newPostElement.current) {
+            const text = newPostElement.current.value;
+            alert(text)
+        }
+    }
 
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea
+                        placeholder={'type message'}
+                        ref={newPostElement}>
+                    </textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
